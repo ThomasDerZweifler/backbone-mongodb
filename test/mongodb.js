99543d8e1b8823782c1986484eb474846e14b1b3
Vadim Mirgorod <dealancer@gmail.com>
2013-05-31 15:56:44 +0300

    test json export

--- test/mongodb.js
+++ test/mongodb.js
@@ -12,12 +12,21 @@ $(document).ready(function() {
 
   var library;
 
+  var attrs = {
+    id: 5,
+    title: "The Tempest",
+    author: "Bill Shakespeare",
+  };
+
   module('Backbone.Mongodb', _.extend(new Environment, {
 
     setup : function() {
 
       // Create new library
       library = new Library();
+
+      // Define basic attrs
+      library.create(attrs, {wait: false});
 
       // Provide mockup AJAX responses.
       $.mockjax({
@@ -68,4 +77,19 @@ $(document).ready(function() {
     }, 50);
   });
 
+  asyncTest("Read MongoDB Extended JSON", 1, function() {
+    library.fetch();
+
+    setTimeout(function() {
+      ok(library.get('10'));
+      start();
+    }, 50);
+  });
+
+  test("Export to MongoDB Extended JSON", 2, function(){
+    var book = library.get(5);
+    ok(book);
+    var json = book.toJSON();
+    equal(json._id.$oid, 5);
+  });
 });
