9fd7aaba5de4b34d042ad72678341972d26af454
Vadim Mirgorod <dealancer@gmail.com>
2013-05-31 13:47:49 +0300

    updated the way how MongoModel is extended

--- backbone-mongodb.js
+++ backbone-mongodb.js
@@ -6,30 +6,29 @@
 (function(Backbone) {
 
   // Define mixing that we will use in our extension.
-  var mixin = function() {
-    return {
-      // Convert MongoDB Extended JSON into regular one.
-      parse: function(resp, options) {
-        if (_.isObject(resp._id))  {
-          resp.id = resp._id.$oid;
-          delete resp._id;
-        }
-        return resp;
-      },
+  var mixin = {
 
-      // Convert regular JSON into MongoDB extended one.
-      toJSON: function() {
-        var attrs = _.omit(this.attributes, 'id');
-        if (!_.isUndefined(this.id))  {
-          attrs._id = { $oid: this.id };
-        }
-        return attrs;
+    // Convert MongoDB Extended JSON into regular one.
+    parse: function(resp, options) {
+      if (_.isObject(resp._id))  {
+        resp.id = resp._id.$oid;
+        delete resp._id;
       }
+      return resp;
+    },
+
+    // Convert regular JSON into MongoDB extended one.
+    toJSON: function() {
+      var attrs = _.omit(this.attributes, 'id');
+      if (!_.isUndefined(this.id))  {
+        attrs._id = { $oid: this.id };
+      }
+      return attrs;
     }
   }
 
   // Create new MongoModel object.
-  Backbone.MongoModel = _.extend(Backbone.Model, mixin);
+  Backbone.MongoModel = Backbone.Model.extend(mixin);
 
   // Provide mixin to extend Backbone.Model.
   Backbone.MongoModel.mixin = mixin;
